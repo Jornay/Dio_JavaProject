30178f1cc16425d414b6e3ad2cfa25e19561285e
Jornay <lucasdeafg@yahoo.com.br>
2021-07-05 19:06:36 -0300

    Create Person Created

--- personapi/src/main/java/one/digitalInnovation/personapi/controllers/PersonController.java
+++ personapi/src/main/java/one/digitalInnovation/personapi/controllers/PersonController.java
@@ -1,16 +1,35 @@
 package one.digitalInnovation.personapi.controllers;
 
 import lombok.AllArgsConstructor;
+import one.digitalInnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalInnovation.personapi.entities.Person;
+import one.digitalInnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/api/v1/people")
-@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson( @RequestBody  Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID: " + savedPerson.getId() )
+                .build();
+    }
 
 
 }
