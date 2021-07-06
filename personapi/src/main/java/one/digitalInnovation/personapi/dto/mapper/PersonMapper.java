0d06db1e396b81eff61516796ff7f0a16bec8230
Jornay <lucasdeafg@yahoo.com.br>
2021-07-06 19:14:49 -0300

    Adding DTOs and ListAll controller

--- personapi/src/main/java/one/digitalInnovation/personapi/dto/mapper/PersonMapper.java
+++ personapi/src/main/java/one/digitalInnovation/personapi/dto/mapper/PersonMapper.java
@@ -4,9 +4,13 @@ import one.digitalInnovation.personapi.dto.request.PersonDTO;
 import one.digitalInnovation.personapi.entities.Person;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+
+@Mapper
 public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
     @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
     Person toModel(PersonDTO dto);
